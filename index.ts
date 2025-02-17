a5c0c53dae14aead98dce5f84cfb431a5034dfbc
gene9831 <pacify.98@gmail.com>
2025-02-17 14:33:28 +0800

    update

--- index.ts
+++ index.ts
@@ -1,10 +1,87 @@
 import type { Request, Response } from 'express'
 import express from 'express'
-import {
-  createProxyMiddleware,
-  responseInterceptor,
-} from 'http-proxy-middleware'
-import { parseStringPromise, Builder } from 'xml2js'
+import http from 'http'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import { Builder, parseStringPromise } from 'xml2js'
+
+const xmlBuilder = new Builder({
+  renderOpts: { pretty: false, indent: '', newline: '' },
+})
+
+const handlePropfind = (
+  proxyRes: http.IncomingMessage,
+  req: Request,
+  res: Response,
+) => {
+  const buffers: Buffer[] = []
+
+  proxyRes.on('data', (chunk) => {
+    buffers.push(chunk)
+  })
+
+  proxyRes.on('end', async () => {
+    const data = Buffer.concat(buffers)
+
+    try {
+      const result = await parseStringPromise(data.toString('utf-8'))
+      const responses = result?.['D:multistatus']?.['D:response']
+
+      if (!Array.isArray(responses)) {
+        throw new Error()
+      }
+
+      const metaDataResp = responses.find((item) =>
+        item['D:href'][0].endsWith('encrypted.json'),
+      )
+      const encryptedDataResp = responses.find((item) =>
+        item['D:href'][0].endsWith('encrypted.mp4'),
+      )
+
+      if (!metaDataResp || !encryptedDataResp) {
+        throw new Error()
+      }
+
+      const url = new URL(webdavTarget)
+      url.pathname = metaDataResp['D:href']
+
+      const resp = await fetch(url, {
+        method: 'GET',
+        headers: {
+          authorization: req.headers.authorization || '',
+        },
+      })
+
+      const metaData = await resp.json()
+
+      const copied = structuredClone(encryptedDataResp)
+
+      copied['D:href'][0] = (copied['D:href'][0] as string)
+        .split('/')
+        .slice(0, -1)
+        .concat(encodeURIComponent(metaData.filename))
+        .join('/')
+
+      if (copied['D:propstat'][0]['D:prop'][0]['D:displayname']?.[0]) {
+        copied['D:propstat'][0]['D:prop'][0]['D:displayname'][0] =
+          metaData.filename
+      }
+
+      // TODO 还需要处理文件的 PROPFIND 和 GET
+      responses.push(copied)
+
+      console.log(JSON.stringify(responses))
+
+      const newData = xmlBuilder.buildObject(result)
+
+      res.write(newData, 'utf-8')
+    } catch (err) {
+      // console.log(err)
+      res.write(data)
+    } finally {
+      res.end()
+    }
+  })
+}
 
 const app = express()
 
@@ -18,75 +95,28 @@ app.use(
     target: webdavTarget,
     changeOrigin: true, // 修改请求头中的 Origin 字段，使其指向目标 WebDAV 服务器
     selfHandleResponse: true,
+    logger: console,
     on: {
       proxyReq: (proxyReq, req, res) => {
-        console.log(`代理请求: ${req.method} ${req.url}`)
+        console.log(req.headers)
       },
-      proxyRes: responseInterceptor(
-        async (responseBuffer, proxyRes, req, res) => {
-          if (req.headers['range']) {
-            console.log(req.headers)
-            console.log(proxyRes.statusCode)
-            console.log(proxyRes.headers)
-          }
-          // TODO 条件
-          // 1. req.headers有range
-          // 2. proxyRes.statusCode 为 3xx
-          // 3. proxyRes.headers有location
-          // 如何返回stream
+      proxyRes: (proxyRes, req, res) => {
+        // 手动处理响应需要写入3部份数据：状态码、响应头部、响应体
+        res.status(proxyRes.statusCode || 404)
 
-          if (!/xml/.test(proxyRes.headers['content-type'] || '')) {
-            console.log(proxyRes.headers['content-type'])
-            console.log(responseBuffer.length)
-            return responseBuffer
-          }
+        Object.entries(proxyRes.headers).forEach(([key, value]) => {
+          value && res.setHeader(key, value)
+        })
 
-          // "text/xml; charset=utf-8"
-          const data = responseBuffer.toString('utf-8')
-
-          try {
-            // 解析 XML 数据
-            const result = await parseStringPromise(data)
-
-            // 在 XML 中新增一个目录响应
-            const testDirectory = {
-              'D:href': ['/dav/ProxyTest/'],
-              'D:propstat': [
-                {
-                  'D:prop': [
-                    {
-                      'D:resourcetype': [
-                        { 'D:collection': [{ $: { 'xmlns:D': 'DAV:' } }] },
-                      ],
-                      'D:displayname': ['ProxyTest'],
-                    },
-                  ],
-                  'D:status': ['HTTP/1.1 200 OK'],
-                },
-              ],
-            }
-
-            if (result['D:multistatus']) {
-              const response = result['D:multistatus']['D:response']
-              if (Array.isArray(response)) {
-                result['D:multistatus']['D:response'] =
-                  response.concat(testDirectory)
-              } else {
-                result['D:multistatus']['D:response'] = [testDirectory]
-              }
-            }
-
-            const newData = new Builder({
-              renderOpts: { pretty: false, indent: '', newline: '' },
-            }).buildObject(result)
-
-            return newData
-          } catch (err) {
-            console.error('解析 XML 错误', err)
-            return data
-          }
-        },
-      ),
+        if (
+          req.method.toUpperCase() === 'PROPFIND' &&
+          proxyRes.statusCode === 207
+        ) {
+          handlePropfind(proxyRes, req, res)
+        } else {
+          proxyRes.pipe(res)
+        }
+      },
     },
   }),
 )
