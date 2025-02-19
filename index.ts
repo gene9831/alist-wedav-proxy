cf5310ebab029ad2a026143b1b04a1162a2c0c7e
gene9831 <pacify.98@gmail.com>
2025-02-19 15:54:04 +0800

    TODO handle GET request

--- index.ts
+++ index.ts
@@ -9,6 +9,122 @@ function escapeRegex(string: string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // 转义所有特殊字符
 }
 
+interface EncContext {
+  path: string
+  originHref: string
+  filename: string
+}
+
+const encContextMaps = new Map<string, EncContext>()
+
+const renameDAVResultResponse = (
+  davResp: DAVResultResponse,
+  newName: string,
+) => {
+  const copied = structuredClone(davResp)
+  copied.href = copied.href
+    .split('/')
+    .slice(0, -1)
+    .concat(encodeURIComponent(newName))
+    .join('/')
+
+  encContextMaps.set(copied.href.replace(/^\/dav/, ''), {
+    path: davResp.href.replace(/^\/dav/, ''),
+    originHref: davResp.href,
+    filename: newName,
+  })
+
+  if (copied.propstat?.prop.displayname) {
+    copied.propstat.prop.displayname = newName
+  }
+
+  return copied
+}
+
+const handlePropfindList = async (result: DAVResult, req: Request) => {
+  const responses = result.multistatus.response
+
+  // 匹配例如 xxx-enc-key.json
+  const keyNameRegExp = /^(.+-)?enc-key\.json$/
+  // 匹配例如 xxx-enc-data.mp4
+  const dataNameRegExp = /^(.+-)?enc-data\.[a-zA-Z0-9]+$/
+
+  const encKeyResps: DAVResultResponse[] = []
+  const encDataResps: DAVResultResponse[] = []
+  const normalResps: DAVResultResponse[] = []
+
+  for (const resp of responses) {
+    if (
+      keyNameRegExp.test(resp.href) &&
+      !resp.propstat?.prop.resourcetype.collection
+    ) {
+      encKeyResps.push(resp)
+    } else if (
+      dataNameRegExp.test(resp.href) &&
+      !resp.propstat?.prop.resourcetype.collection
+    ) {
+      encDataResps.push(resp)
+    } else {
+      normalResps.push(resp)
+    }
+  }
+
+  if (encKeyResps.length === 0) {
+    throw new Error('Encrypted data not found')
+  }
+
+  const encPairResps: {
+    key: DAVResultResponse
+    data: DAVResultResponse
+  }[] = []
+
+  for (const keyResp of encKeyResps) {
+    const prefix = keyResp.href.replace('enc-key.json', '')
+    const regExp = new RegExp(`^${escapeRegex(prefix)}enc-data\\.[a-zA-Z0-9]+$`)
+    const dataResp = encDataResps.find((item) => regExp.test(item.href))
+    if (dataResp) {
+      encPairResps.push({ key: keyResp, data: dataResp })
+    }
+  }
+
+  for (const { key, data } of encPairResps) {
+    const url = new URL(webdavTarget)
+    url.pathname = key.href
+
+    const resp = await fetch(url, {
+      method: 'GET',
+      headers: { authorization: req.headers.authorization || '' },
+    })
+
+    const metaData = await resp.json()
+
+    const newResp = renameDAVResultResponse(data, metaData.filename)
+
+    // TODO 还需要处理文件的 GET
+    normalResps.push(newResp)
+  }
+
+  return jsonObj2XML({
+    multistatus: { response: normalResps },
+  })
+}
+
+const handlePropfindEnc = (result: DAVResult, req: Request) => {
+  const responses = result.multistatus.response
+
+  const newResps = responses.map((item) => {
+    const encContext = req.context!.encContext
+    if (item.href === encContext.originHref) {
+      return renameDAVResultResponse(item, encContext.filename)
+    }
+    return item
+  })
+
+  return jsonObj2XML({
+    multistatus: { response: newResps },
+  })
+}
+
 const handlePropfind = (
   proxyRes: http.IncomingMessage,
   req: Request,
@@ -27,98 +143,36 @@ const handlePropfind = (
       const xmlStr = data.toString('utf-8')
 
       if (!validateXML(xmlStr)) {
-        throw new Error('InvalidXML')
+        throw new Error('Invalid XML')
       }
 
       const result = await parseXML(xmlStr)
-      const responses = result.multistatus.response
 
-      // 匹配例如 xxx-enc-key.json
-      const keyNameRegExp = /^(.+-)?enc-key\.json$/
-      // 匹配例如 xxx-enc-data.mp4
-      const dataNameRegExp = /^(.+-)?enc-data\.[a-zA-Z0-9]+$/
-
-      const encKeyResps: DAVResultResponse[] = []
-      const encDataResps: DAVResultResponse[] = []
-      const normalResps: DAVResultResponse[] = []
-
-      for (const resp of responses) {
-        if (
-          keyNameRegExp.test(resp.href) &&
-          !resp.propstat?.prop.resourcetype.collection
-        ) {
-          encKeyResps.push(resp)
-        } else if (
-          dataNameRegExp.test(resp.href) &&
-          !resp.propstat?.prop.resourcetype.collection
-        ) {
-          encDataResps.push(resp)
-        } else {
-          normalResps.push(resp)
-        }
+      let newData: string
+      if (req.context?.enc) {
+        newData = handlePropfindEnc(result, req)
+      } else {
+        newData = await handlePropfindList(result, req)
       }
-
-      if (encKeyResps.length === 0) {
-        throw new Error()
-      }
-
-      const encPairResps: {
-        key: DAVResultResponse
-        data: DAVResultResponse
-      }[] = []
-
-      for (const keyResp of encKeyResps) {
-        const prefix = keyResp.href.replace('enc-key.json', '')
-        const regExp = new RegExp(
-          `^${escapeRegex(prefix)}enc-data\\.[a-zA-Z0-9]+$`,
-        )
-        const dataResp = encDataResps.find((item) => regExp.test(item.href))
-        if (dataResp) {
-          encPairResps.push({ key: keyResp, data: dataResp })
-        }
-      }
-
-      for (const { key, data } of encPairResps) {
-        const url = new URL(webdavTarget)
-        url.pathname = key.href
-
-        const resp = await fetch(url, {
-          method: 'GET',
-          headers: { authorization: req.headers.authorization || '' },
-        })
-
-        const metaData = await resp.json()
-
-        const copied = structuredClone(data)
-
-        copied.href = copied.href
-          .split('/')
-          .slice(0, -1)
-          .concat(encodeURIComponent(metaData.filename))
-          .join('/')
-
-        if (copied.propstat?.prop.displayname) {
-          copied.propstat.prop.displayname = metaData.filename
-        }
-
-        // TODO 还需要处理文件的 PROPFIND 和 GET
-        normalResps.push(copied)
-      }
-
-      const newResult: DAVResult = {
-        multistatus: { response: normalResps },
-      }
-
-      const newData = jsonObj2XML(newResult)
 
       res.write(newData, 'utf-8')
     } catch (err) {
-      // console.log(err)
       res.write(data)
     } finally {
       res.end()
     }
   })
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      context?: {
+        enc: boolean
+        encContext: EncContext
+      } & Record<string, any>
+    }
+  }
 }
 
 const app = express()
@@ -134,10 +188,15 @@ app.use(
     changeOrigin: true, // 修改请求头中的 Origin 字段，使其指向目标 WebDAV 服务器
     selfHandleResponse: true,
     logger: console,
+    pathRewrite: (path, req) => {
+      const encContext = encContextMaps.get(path)
+      if (encContext) {
+        req.context = { enc: true, encContext: encContext }
+        return encContext.path
+      }
+      return path
+    },
     on: {
-      proxyReq: (proxyReq, req, res) => {
-        console.log(req.headers)
-      },
       proxyRes: (proxyRes, req, res) => {
         // 手动处理响应需要写入3部份数据：状态码、响应头部、响应体
         res.status(proxyRes.statusCode || 404)
